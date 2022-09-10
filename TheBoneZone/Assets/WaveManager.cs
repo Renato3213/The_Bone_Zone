101d41888a63ab4c24f8f21632b087f558db7219
Renato3213 <renatoscatolin9@gmail.com>
2022-09-10 18:15:29 -0300

    wave_prototype

---
+++ TheBoneZone/Assets/WaveManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] bool northActive, southActive, westActive, eastActive;
+    [SerializeField] float timer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
