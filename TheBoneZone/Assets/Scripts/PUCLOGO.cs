92531e6fc1998d830a93674d7129e9ee40f94e1d
Renato3213 <118687843+GemaLuzente@users.noreply.github.com>
2022-11-24 23:56:09 -0300

    Soninho Gostoso
    hummm

--- TheBoneZone/Assets/Scripts/PUCLOGO.cs
+++ TheBoneZone/Assets/Scripts/PUCLOGO.cs
@@ -1,16 +1,14 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PUCLOGO : MonoBehaviour
 {
-    public Image img;
     private void Start()
     {
         Invoke("StartGame", 2);
     }
     private void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Menu");
     }
 }
