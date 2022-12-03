4522f765d2dc030131e6fa47139a5171a55251c4
Renato3213 <118687843+GemaLuzente@users.noreply.github.com>
2022-12-02 21:45:53 -0300

    Menus +

---
+++ TheBoneZone/Assets/rotate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float speed;
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, speed * Time.fixedDeltaTime, 0);
+    }
+}
