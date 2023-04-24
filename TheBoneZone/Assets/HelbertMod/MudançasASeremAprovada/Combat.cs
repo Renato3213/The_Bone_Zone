db314afad41c7585a37c495ffd7cf8c450c1334e
Helbert Felix <112450278+HelbertFelix@users.noreply.github.com>
2023-04-23 21:11:33 -0300

    Atualizei o combate para todos os dados.

--- TheBoneZone/Assets/HelbertMod/MudançasASeremAprovada/Combat.cs
+++ TheBoneZone/Assets/HelbertMod/MudançasASeremAprovada/Combat.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
+    //Referência da magia do jogador.
     public ISpell selectedSpell;
     //Turno de ataque.
     [SerializeField] bool attackTurn = false;
     //Variável de controle da quantidade de dados por esqueletos.
     const int skeletonGroup = 5;
+    //Indidacador de qual rodada do turno está.
+    int round;
+    //Quantidade máxima rodadas por turno.
+    int maxRounds;
 
     [Header("Player Variables")]
     //Quantidade de dados do jogador.
@@ -17,6 +22,10 @@ public class Combat : MonoBehaviour
     public int playerDiceNumber;
     //Quantidade de esqueletos do jogador.
     public int amountPlayerSkeletons;
+    //Lita de dados do jogador.
+    public List<int> playerDice = new List<int>();
+    //Número de lados do dado do jogador.
+    const int sideDicePlayer = 7;
 
     [Header("NPC Variables")]
     //Quantidade de dados da máquina.
@@ -25,21 +34,71 @@ public class Combat : MonoBehaviour
     public int npcDiceNumber;
     //Quantidade de esqueletos da máquina.
     public int amountNpcSkeletons;
-    
+    //Lista de dados do npc
+    public List<int> opponentDice = new List<int>();
+    //Número de lados do dado do jogador.
+    public int sideDiceNpc = 7;
+
+    //Informa a Magia selecionada.
+    //void SetSpell()
+    //{
+        //Indica a magia selecionada.
+        //this.selectedSpell = GameManager.instance.spell;
+    //}
+
     public void StartCombat(int targetedSkeletons)
     {
-        //Atribui a quantidade de esqueletos do jogador
+        //SetSpell();
+        //Atribui a quantidade de esqueletos do jogador.
         amountPlayerSkeletons = targetedSkeletons;
-        //Calcula e indica a quantidade de dados do jogador;
+        //Calcula e indica a quantidade de dados do jogador.
         amountPlayerDice = targetedSkeletons / skeletonGroup;
-        //Atribui a mesma quantidade de dados do jogador para a máquina.
-        amountNpcDice = amountPlayerDice;
+        //Calcula e indica a quantidade de dados da máquina.
+        amountNpcDice = amountNpcSkeletons / skeletonGroup;
+
+        //Define a quantidade de lados do dado da máquina.
+        switch(selectedSpell)
+        {
+            case Fog:
+                sideDiceNpc = 6;
+                break;
+            default:
+                sideDiceNpc = 7;
+                break;
+        }
+
         //Verifica se jogador e máquina tem dados disponíveis.
         if(GetDice())
         {
             //Chama o método que inicia o combate.
             NewTurn();
         }
+    }
+
+    //Adiciona 1 dado ao jogador, sendo "i" a quantidade de lados do dado.
+    void AddDicePlayer(int i)
+    {
+        playerDice.Add(i);
+    }
+
+    //Adiciona 1 dado ao npc, sendo "i" a quantidade de lados do dado.
+    void AddDiceOpponent(int i)
+    {
+        opponentDice.Add(i);
+    }
+
+    //Remove o dado da primeira posição do jogador.
+    void RemoveDicePlayer()
+    {
+        Debug.Log("Remove dado Player");
+        playerDice.RemoveAt(0);
+    }
+
+    //Remove o dado da primeira posição do npc.
+    void RemoveDiceOpponent()
+    {
+        Debug.Log("Remove dado Adversario");
+        opponentDice.RemoveAt(0);
     }
     
     //Método que faz a verificação se ambos tem dados.
@@ -51,6 +110,10 @@ public class Combat : MonoBehaviour
     //Método responsável por chama a mudança de turno e o sorteio dos números nos dados.
     void NewTurn()
     {
+        //Remove todos os dados antes de cada turno.
+        playerDice.Clear();
+        opponentDice.Clear();
+
         //Chama o método que alterna entre o turno de ataque e defesa.
         ChangeTurn();
         //Chama o método que faz o sorteio dos números dos dados.
@@ -67,21 +130,72 @@ public class Combat : MonoBehaviour
     //Método que sorteia os números dos dados.
     void DiceDraw()
     {
-        //Sorteia o dado do jogador.
-        playerDiceNumber = Random.Range(1, 7);
-        //Sorteia o dado da máquina.
-        npcDiceNumber = Random.Range(1, 7);
+        //Atribui à ambas listas a quantidade de dados de cada uma.
+        for(int i = 0; i < amountPlayerDice; i++)
+        {
+            AddDicePlayer(sideDicePlayer);
+        }
+        for(int i = 0; i < amountNpcDice; i++)
+        {
+            AddDiceOpponent(sideDiceNpc);
+        }
+
+        //Define o número máximo de rodadas do turno baseado de acordo com quem tem a maior quantidade de dados.
+        maxRounds = (playerDice.Count > opponentDice.Count)? playerDice.Count:opponentDice.Count;
+        //Redefine a rodada para 0;
+        round = 0;
+
+        //Condicional responsável por comparar os resultados do sorteio dos dados e remover o dado do derrotado na rodada.
+        while(round < maxRounds && opponentDice.Count > 0 && playerDice.Count > 0)
+        {
+            Debug.Log("max: " + maxRounds + "(" + round + ")");
+            
+            //Sorteio do número do dado do jogador.(Número Aleátorio(0, ValorMáximoDoDado[PosiçãoDoDado = NúmeroDaRodada % QuantidadeDeDadosDoJogador]))
+            playerDiceNumber = Random.Range(1, playerDice[round % playerDice.Count]);
+            //Sorteio do número do dado da máquina.(Número Aleátorio(0, ValorMáximoDoDado[PosiçãoDoDado = NúmeroDaRodada % QuantidadeDeDadosDaMáquina]))
+            npcDiceNumber = Random.Range(1, opponentDice[round % opponentDice.Count]);
+            Debug.Log("Jogador: " + playerDiceNumber + "  |   Adversario: " + npcDiceNumber);
+
+            //Compara se deu empate nos dados.
+            if(playerDiceNumber == npcDiceNumber)
+            {
+                Debug.Log("Empate");
+                //Se dá empate na rodada e for turno de ataque remove o dado do jogador, caso seja o turno de defesa remove o dado da máquina.
+                if(attackTurn)
+                {
+                    RemoveDicePlayer();
+                }
+                else
+                {
+                    RemoveDiceOpponent();
+                }
+            }
+            //Se o número do dado do jogador for maior remove um dado da máquina.
+            else if(playerDiceNumber > npcDiceNumber)
+            {
+                Debug.Log("Player ganha");
+                RemoveDiceOpponent();
+            }
+            //Se o número do dado da máquina for maior remove um dado do jogador.
+            else
+            {
+                Debug.Log("Adversário ganha");
+                RemoveDicePlayer();
+            }
+            round++;
+        }
+
         //Chama o método que compara os resultados dos dados.
         CompareDice();
     }
     
-    //Método responsável por comparar os resultados do sorteio dos dados e remover o dado do derrotado no turno.
+    //Método que verifica quem foi o vencedor ao final do turno.
     void CompareDice()
     {
-        //Compara se deu empate nos dados.
-        if(npcDiceNumber == playerDiceNumber)
+        //Compara se deu empate no turno.
+        if(playerDiceNumber == npcDiceNumber)
         {
-            //Se dá empate no turno de ataque remove o dado do jogador, caso seja o turno de defesa remove o dado da máquina.
+            //Se dá empate e seja o turno de ataque remove o dado do jogador, caso seja o turno de defesa remove o dado da máquina.
             if(attackTurn)
             {
                 amountPlayerDice -= 1;
@@ -91,12 +205,12 @@ public class Combat : MonoBehaviour
                 amountNpcDice -= 1;
             }
         }
-        //Se o número do dado da máquina for maior remove um dado do jogador.
-        else if(npcDiceNumber > playerDiceNumber)
+        //Remove o dado do jogador caso seja o perdedor do turno.
+        else if(opponentDice.Count > playerDice.Count)
         {
             amountPlayerDice -= 1;
         }
-        //Se o número do dado do jogador for maior remove um dado da máquina.
+        //Remove o dado da máquina caso seja o perdedor do turno.
         else
         {
             amountNpcDice -= 1;
